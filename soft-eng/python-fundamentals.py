a2d7e09303aca9870772b3caeeb65dc284476b1a
Jeff <unvedicus@gmail.com>
2025-04-26 10:43:23 +0800

    feat: added python fundamentals

---
+++ soft-eng/python-fundamentals.py
@@ -0,0 +1,52 @@
+# Write your code for Exercise 1 in this cell
+print('Exercise 1')
+print('This is my first program')
+
+# Write your code for Exercise 2 in this cell
+print('\nExercise 2')
+myint = 200
+print(myint)
+
+# Write your code for Exercise 3 in this cell
+print('\nExercise 3')
+a = 20
+c = a * 100
+print(c)
+
+# Write your code for Exercise 4 in this cell
+print('\nExercise 4')
+myval = 8
+if myval > 0:
+    print("It is a positive number")
+else:
+    print("It is a negative number")
+
+# Write your code for Exercise 5 in this cell
+print('\nExercise 5')
+myarray = [20, 10, 44, 928, 200]
+for x in myarray:
+    print(x)
+
+# Write your code for Exercise 6 in this cell
+print('\nExercise 6')
+shopping_list = ["bread", "water", "chips", "candy", "soda"]
+for x in shopping_list:
+    print(x)
+    if x == "chips":
+        break
+
+# Write your code for Exercise 7 in this cell
+print('\nExercise 7')
+Score=60
+while Score<90:
+    print(Score)
+    Score += 5
+
+# Write your code for Exercise 8 in this cell
+print('\nExercise 8')
+myval = 45
+while myval < 90:
+    print(myval)
+    if myval > 80:
+        break
+    myval += 8
